faead2ea18225e8fc599a77fc8e73854d66de787
Dimitri Couwenberg <dimitri@192-168-1-101.tpgi.com.au>
2017-06-25 10:51:40 +1000

    first mortgage chart

--- app/components/testchart.jsx
+++ app/components/testchart.jsx
@@ -6,13 +6,40 @@ var LineChart = require('react-d3-basic').LineChart;
 export default class Test extends Component {
 
 
+    generateData(interest,balance,duration) {
+
+        let data = [];
+
+        //interest
+
+        //balance
+
+        //duration per year
+
+
+        for (var i = 1; i <= duration; i++) {
+
+            let year ={};
+
+            //todo use proper formula
+            year.balance =balance - i * (balance/duration)  ;
+            year.yr=duration -(i+1);
+
+            data.push(year);
+
+        }
+
+
+        return data;
+    }
+
     render() {
 
 
         let chartData = [
             {
                 name: "Lavon Hilll I",
-                BMI: 20.57,
+                BMI: 120.57,
                 age: 12,
                 birthday: "1994-10-26T00:00:00.000Z",
                 city: "Annatown",
@@ -59,14 +86,14 @@ export default class Test extends Component {
             // color: what color is the line
             chartSeries = [
                 {
-                    field: 'index',
+                    field: 'balance',
                     name: 'BMI',
                     color: '#ff7f0e'
                 }
             ],
             // your x accessor
             x = function (d) {
-                return d.age;
+                return d.yr;
             }
 
         return (<div>
@@ -75,7 +102,7 @@ export default class Test extends Component {
                 <LineChart
                     margins={margins}
                     title={title}
-                    data={chartData}
+                    data={this.generateData(0,200000,10)}
                     width={width}
                     height={height}
                     chartSeries={chartSeries}
